8d7f0991e53b927301417ca3c75a20d1e65db2b4
boonpat.papob <boonpat.papob@gmail.com>
2022-01-04 17:17:18 +0700

    Update get window

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,22 @@
+import time
 from flask import Flask, request
-
+from multiprocessing import Process
+from AppKit import NSWorkspace
 app =Flask(__name__)
 
 @app.route("/hello", methods=['GET'])
 def hello():
   return "20"
 
+def mainLoop():
+  # while True:
+  #   activeAppName = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationPath']
+  #   print (activeAppName)
+  #   time.sleep(5)
+
+    
 if __name__ == "__main__":
+  p = Process(target=mainLoop)
+  p.start()
   app.run(debug=True, port=5000)
+  p.join()
